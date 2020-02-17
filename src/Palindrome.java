c99271bca826bab82c60db8c2ffc39a2daa3db56
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2020-02-17 22:52:56 +0800

    Clean code for palindrome test

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -6,11 +6,11 @@
 public class Palindrome {
 
     public static void main(String args[]) {
-        System.out.println("Is aaa palindrome?: " + isPalindromString("aaa"));
-        System.out.println("Is abc palindrome?: " + isPalindromString("abc"));
+        System.out.println("Is aaa palindrome?  : " + isPalindrome("aaa"));
+        System.out.println("Is abc palindrome?  : " + isPalindrome("abc"));
 
-        System.out.println("Is bbbb palindrome?: " + isPalindromString("bbbb"));
-        System.out.println("Is defg palindrome?: " + isPalindromString("defg"));
+        System.out.println("Is bbbb palindrome? : " + isPalindrome("bbbb"));
+        System.out.println("Is defg palindrome? : " + isPalindrome("defg"));
     }
 
     /**
@@ -19,12 +19,7 @@ public class Palindrome {
      * @param text
      * @return true if text is palindrome, otherwise false
      */
-    public static boolean isPalindromString(String text) {
-        String reverse = reverse(text);
-        if (text.equals(reverse)) {
-            return true;
-        }
-
+    public static boolean isPalindrome(String text) {
         return false;
     }
 
@@ -35,11 +30,6 @@ public class Palindrome {
      * @return reversed String of input
      */
     public static String reverse(String input) {
-        if (input == null || input.isEmpty()) {
-            return input;
-        }
-
-        return input.charAt(input.length() - 1) + reverse(input.substring(0, input.length() - 1));
+        return null;
     }
-
 }
